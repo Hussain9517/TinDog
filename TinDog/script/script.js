c9a7e5e2e495260b87e8065adf18d2175222f7c2
MUHAMMAD ALI HUSSAIN <80855891+Hussain9517@users.noreply.github.com>
2022-09-03 04:42:03 -0700

    Add files via upload

---
+++ TinDog/script/script.js
@@ -0,0 +1,87 @@
+
+
+
+if (window.matchMedia('(width: 414px)').matches)
+{
+    $(document).ready(function(){
+        $('.navbar-toggler-icon').click(function(){
+            $('.box-margin1').css('margin-top', '170px');
+            $('.dog-img').css('width','240px');
+            })
+        
+    })
+}
+
+
+if (window.matchMedia('(width: 412px)').matches)
+{
+    $(document).ready(function(){
+        $('.navbar-toggler-icon').click(function(){
+            $('.box-margin1').css('margin-top', '170px');
+            $('.dog-img').css('width','240px');
+            })
+        
+    })
+}
+
+
+if (window.matchMedia('(width: 393px)').matches)
+{
+    $(document).ready(function(){
+        $('.navbar-toggler-icon').click(function(){
+            $('.box-margin1').css('margin-top', '185px');
+            $('.dog-img').css('width','230px');
+            })
+        
+    })
+}
+
+
+if (window.matchMedia('(width: 390px)').matches)
+{
+    $(document).ready(function(){
+        $('.navbar-toggler-icon').click(function(){
+            $('.box-margin1').css('margin-top', '185px');
+            $('.dog-img').css('width','230px');
+            })
+        
+    })
+}
+
+
+
+if (window.matchMedia('(width: 384px)').matches)
+{
+    $(document).ready(function(){
+        $('.navbar-toggler-icon').click(function(){
+            $('.box-margin1').css('margin-top', '185px');
+            $('.dog-img').css('width','230px');
+            })
+        
+    })
+}
+
+
+
+if (window.matchMedia('(width: 375px)').matches)
+{
+    $(document).ready(function(){
+        $('.navbar-toggler-icon').click(function(){
+            $('.box-margin1').css('margin-top', '185px');
+            $('.dog-img').css('width','220px');
+            })
+        
+    })
+}
+
+
+if (window.matchMedia('(width: 360px)').matches)
+{
+    $(document).ready(function(){
+        $('.navbar-toggler-icon').click(function(){
+            $('.box-margin1').css('margin-top', '185px');
+            $('.dog-img').css('width','210px');
+            })
+        
+    })
+}
